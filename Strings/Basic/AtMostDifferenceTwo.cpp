77aa578a07b18aae16f43ea97065bcaeeec79d9f
cookiepro8 <abdulrazakr111@gmail.com>
2026-06-07 11:43:00 +0530

    .

---
+++ Strings/Basic/AtMostDifferenceTwo.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        for(int i = 1; i<s.size(); i++){
+            int diff = abs((s[i] - '0') - (s[i-1] - '0'));
+            if(diff > 2) return false;
+        }
+        return true;
+    }
+};
