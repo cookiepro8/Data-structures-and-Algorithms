04c354266d52133c108a662e2999d396037f0060
cookiepro8 <abdulrazakr111@gmail.com>
2026-06-09 12:36:14 +0530

    3689

---
+++ maximumSubarrayTotalValue I.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int n = nums.size();
+        int gMin = nums.front(), gMax = nums.front();
+
+        for(auto &n: nums){
+            gMin = min(gMin, n);
+            gMax = max(gMax, n);
+        }
+        return (long long) (gMax - gMin) * k;
+    }
+};
