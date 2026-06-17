cfa75300ed124815e062809f552bf48c19bb1b13
cookiepro8 <abdulrazakr111@gmail.com>
2026-06-17 09:36:11 +0530

    Leetcode 1870

---
+++ BinarySearch/Bs on answers/MinSpeedToArriveOnTime.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    double totalTime(vector<int>& dist, int speed) {
+        double tt = 0;
+        for(int i = 0; i<dist.size()-1; i++){
+            tt += ceil((double)dist[i]/ speed);
+        }
+        tt += (double)dist.back()/speed;
+
+        return tt;
+    }
+
+public:
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int low = 1, high = 1e7;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int tt = totalTime(dist, mid);
+
+            if (tt <= hour) {
+                ans = mid;
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+
+        return ans;
+    }
+};
